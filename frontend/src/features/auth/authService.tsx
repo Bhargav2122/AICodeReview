b8630a2d0fb96724b858d91539d1ee42b0de1e4a
Bhargav2122 <balabhargavsurepalli@gmail.com>
2026-01-06 19:49:09 +0530

    fix: api call

--- frontend/src/features/auth/authService.tsx
+++ frontend/src/features/auth/authService.tsx
@@ -2,12 +2,12 @@ import api from "../../api/axios";
 import type { RegisterPayload, LoginPayload, User } from "../../types/auth";
 
 export const register = async(formData: RegisterPayload): Promise<User> => {
-    const res = await api.post('/auth/signup', formData);
+    const res = await api.post('/api/auth/signup', formData);
     return res.data;
 }
 
 export const login = async(formData: LoginPayload): Promise<User> => {
-    const res = await api.post('/auth/signin', formData);
+    const res = await api.post('/api/auth/signin', formData);
      return res.data;
 }
 
--- frontend/src/features/review/reviewService.tsx
+++ frontend/src/features/review/reviewService.tsx
@@ -2,17 +2,17 @@ import api from "../../api/axios";
 import type { ReviewHistory, ReviewHistoryResponse, ReviewRequest, ReviewResponse } from "../../types/reviewTypes";
 
 export const submitReview = async( code: ReviewRequest): Promise<ReviewResponse> => {
-    const res = await api.post('/ai/review', code);
+    const res = await api.post('/api/ai/review', code);
     return res.data;
 }
 
 export const pastChat = async():Promise<ReviewHistory[]> => {
-    const res = await api.get<ReviewHistoryResponse>('/ai/history');
+    const res = await api.get<ReviewHistoryResponse>('/api/ai/history');
     return res.data.history;
 }
 
 export const singleChat = async(id: string): Promise<ReviewHistory> => {
-    const res = await api.get(`/ai/history/${id}`);
+    const res = await api.get(`/api/ai/history/${id}`);
     return res.data.review;
 
 }
